b8484aa3681883b8133274c25af598e2fdef0b9c
Prashant <rumdali.psn@gmail.com>
2026-03-01 21:18:12 -0500

    DRF implemented

--- backend/backtube1/views.py
+++ backend/backtube1/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+
+from django.contrib.auth.models import Group, User
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from backtube1.serializers import UserSerializer, GroupSerializer
+
 
 # Create your views here.
+@api_view(["GET"])
+def UserView(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def GroupView(request):
+    queryset = Group.objects.all()
+    serializer = GroupSerializer(queryset, many=True)
+    return Response(serializer.data)
